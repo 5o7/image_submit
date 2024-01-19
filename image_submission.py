04fb90e5fa310eca89f5b042ced86656dbe1795a
Scotty <1583918+5o7@users.noreply.github.com>
2024-01-19 07:09:26 -0600

    Add files via upload

---
+++ image_submission.py
@@ -0,0 +1,29 @@
+import requests
+import praw
+
+creds = {"client_id": "X",
+         "client_secret": "X",
+         "password": "X!",
+         "user_agent": "Post an image"
+         "username": "5o7bot"}
+reddit = praw.Reddit(client_id=creds["client_id"],
+                     client_secret=creds["client_secret"],
+                     password=creds["password"],
+                     user_agent=creds["user_agent"],
+                     username=creds["username"])
+
+
+url = "https://www.themoviedb.org/t/p/original/6FfCtAuVAW8XJjZ7eWeLibRLWTw.jpg"
+response = requests.get(url)
+with open("image.jpg", "wb") as f:
+    f.write(response.content)
+
+title = 'My favorite picture'
+image = 'image.jpg'
+reddit.subreddit('5o7bot').submit_image(title, image)
+submissions = []
+for submission in reddit.subreddit('5o7bot').__getattribute__("new")(limit=1):
+    submissions.append(submission)
+
+for submission in submissions:
+    submission.reply_image('image.jpg')
